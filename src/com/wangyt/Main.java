6b97df2b02f12da0e94ef00d1bb89f3d454c5f71
wangyt <wangyt_zzu@163.com>
2020-01-03 15:49:24 +0800

    init

---
+++ src/com/wangyt/Main.java
@@ -0,0 +1,9 @@
+package com.wangyt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("this is an aplication about java_src's knowlages.");
+    }
+}
